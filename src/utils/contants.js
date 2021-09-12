e0ae6ed2816cb6f62079b79fb649741861b5aa16
jkadar186 <jkadar186@gmail.com>
2021-09-12 05:03:05 +0500

    feat: constants added

---
+++ src/utils/contants.js
@@ -0,0 +1,9 @@
+const ipfsUrl = 'https://ipfs.io/ipfs/';
+
+const nonceMsg =
+  'Hi there from Octans NFT! Sign this message to prove you have access to this wallet and we’ll log you in. This won’t cost you any coins.\nTo stop hackers using your wallet, here’s a unique message ID they can’t guess: ';
+
+module.exports = {
+  ipfsUrl,
+  nonceMsg,
+};
